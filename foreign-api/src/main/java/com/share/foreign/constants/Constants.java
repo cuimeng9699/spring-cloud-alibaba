d5847c6fe1c5cdec182ac9f0ddc0e986046d3275
Mr.Cui <18910599699@163.com>
2022-07-20 15:53:07 +0800

    commit: feign commons interfance

---
+++ foreign-api/src/main/java/com/share/foreign/constants/Constants.java
@@ -0,0 +1,16 @@
+package com.share.foreign.constants;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName Constants
+ * @Description
+ * @Author Mr.Cui
+ * @Date 7/20/22 9:46 AM
+ */
+@Component
+public class Constants {
+
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String PAGE_NUM = "pageNum";
+}
